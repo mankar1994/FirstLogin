469ec92593544f5446f877f2b7766df4fe43bbd4
ajit <palajitsingh91@gmail.com>
2022-05-31 03:48:07 +0530

    Add project files.

---
+++ FirstLogin/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using FirstLogin.Areas.Identity.Data;
+using FirstLogin.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(FirstLogin.Areas.Identity.IdentityHostingStartup))]
+namespace FirstLogin.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<FirstLoginDbContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("FirstLoginDbContextConnection")));
+
+                services.AddDefaultIdentity<LoginUser>(options =>
+                {
+
+                    options.SignIn.RequireConfirmedAccount = false;
+                    options.Password.RequireLowercase = false;
+                    options.Password.RequireUppercase = false;
+                })
+                    .AddEntityFrameworkStores<FirstLoginDbContext>();
+            });
+        }
+    }
+}
